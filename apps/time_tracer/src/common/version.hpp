b2956a6fb96efc9a573d8338580a2522096f24a4
camellia <130271834+camellia2077@users.noreply.github.com>
2026-01-28 21:47:09 +0800

    .

--- apps/time_tracer/src/common/version.hpp
+++ apps/time_tracer/src/common/version.hpp
@@ -8,7 +8,7 @@
 namespace AppInfo {
 // 使用 constexpr 和 string_view 可以在编译期确定字符串，效率更高
 // 这也是现代C++的推荐做法
-constexpr std::string_view kVersion = "0.5.4";
+constexpr std::string_view kVersion = "0.5.5";
 constexpr std::string_view kLastUpdated = "2025-01-28";
 } // namespace AppInfo
 
